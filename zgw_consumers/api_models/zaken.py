68da328f4c4c3de18458552e83d0e901b65a09cf
Sergei Maertens <sergei@maykinmedia.nl>
2020-03-18 15:29:56 +0100

    :sparkles: Add missing Zaak.einddatum

--- zgw_consumers/api_models/zaken.py
+++ zgw_consumers/api_models/zaken.py
@@ -15,6 +15,7 @@ class Zaak(ZGWModel):
     zaaktype: str
     registratiedatum: date
     startdatum: date
+    einddatum: Optional[date]
     einddatum_gepland: Optional[date]
     uiterlijke_einddatum_afdoening: Optional[date]
     publicatiedatum: Optional[date]
