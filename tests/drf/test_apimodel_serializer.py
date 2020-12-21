eb03f440207b149a82a6a1899f5d7e5de0331d09
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-21 13:07:23 +0100

    :white_check_mark: Added tests for APIModelSerializer introspection

---
+++ tests/drf/test_apimodel_serializer.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from datetime import date, datetime, time
+from decimal import Decimal
+
+import pytest
+from rest_framework import fields
+
+from zgw_consumers.drf.serializers import APIModelSerializer
+
+
+@dataclass
+class SimpleModel:
+    some_str: str
+    some_date: date
+    some_datetime: datetime
+    some_time: time
+    some_int: int
+    some_float: float
+    some_decimal: Decimal
+    some_bool: bool
+
+
+@pytest.mark.parametrize(
+    "field,expected_type",
+    [
+        ("some_str", fields.CharField),
+        ("some_date", fields.DateField),
+        ("some_datetime", fields.DateTimeField),
+        ("some_time", fields.TimeField),
+        ("some_int", fields.IntegerField),
+        ("some_float", fields.FloatField),
+        # ("some_decimal", fields.DecimalField),
+        ("some_bool", fields.BooleanField),
+    ],
+)
+def test_simple_model_serializer_fields(field: str, expected_type):
+    class Serializer(APIModelSerializer):
+        class Meta:
+            model = SimpleModel
+            fields = (field,)
+
+    serializer = Serializer()
+
+    assert field in serializer.fields
+    assert isinstance(serializer.fields[field], expected_type)
