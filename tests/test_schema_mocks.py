a06d5df7da6a1cacc9a2e4685cde42edc154db20
Sergei Maertens <sergei@maykinmedia.nl>
2020-12-21 12:03:02 +0100

    :white_check_mark: Added tests for schema mocking call

--- tests/test_schema_mocks.py
+++ tests/test_schema_mocks.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+import requests
 
 from zgw_consumers.test import mock_service_oas_get, read_schema
 from zgw_consumers.test.schema_mock import _clear_cache
@@ -27,3 +28,17 @@ def test_read_schema_from_dirs(settings, name):
     _clear_cache()
 
     assert f"name: {name}".encode("utf-8") in schema
+
+
+def test_mock_schema_get(settings, requests_mock):
+    settings.ZGW_CONSUMERS_TEST_SCHEMA_DIRS = [os.path.join(TESTS_DIR, "schemas")]
+    mock_service_oas_get(requests_mock, "https://example.com/api/v1/", "dummy")
+
+    response = requests.get("https://example.com/api/v1/schema/openapi.yaml?v=3")
+
+    assert response.content == b"openapi: 3.0.0\nname: dummy\n"
+    assert requests_mock.call_count == 1
+    assert (
+        requests_mock.last_request.url
+        == "https://example.com/api/v1/schema/openapi.yaml?v=3"
+    )
