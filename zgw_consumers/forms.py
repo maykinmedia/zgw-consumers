ca7ca9c154396e65918820a58a0472bfebabacac
Silvia Amabilino <silvia@maykinmedia.nl>
2022-02-21 16:35:29 +0100

    :sparkles: [#32] Certificate admin form

---
+++ zgw_consumers/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from OpenSSL import crypto
+
+from zgw_consumers.models import Certificate
+
+
+class CertificateAdminForm(forms.ModelForm):
+    class Meta:
+        model = Certificate
+        fields = ("label", "type", "public_certificate", "private_key")
+
+    def clean_public_certificate(self):
+        try:
+            crypto.load_certificate(
+                crypto.FILETYPE_PEM,
+                self.cleaned_data["public_certificate"].encode("utf-8"),
+            )
+        except Exception:
+            raise ValidationError(_("Invalid certificate"), code="invalid")
+        return self.cleaned_data["public_certificate"]
