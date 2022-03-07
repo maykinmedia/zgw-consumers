ca90542132879111de89f7218e4fe79349d6c793
SilviaAmAm <silvia@maykinmedia.nl>
2022-03-07 12:05:38 +0100

    :white_check_mark: [#32] Test mTLS

--- tests/test_certificates.py
+++ tests/test_certificates.py
@@ -4,11 +4,12 @@ from datetime import datetime
 from django.core.files import File
 from django.test import TestCase
 
+import requests_mock
 from privates.test import temp_private_root
 
-from zgw_consumers.constants import CertificateTypes
+from zgw_consumers.constants import APITypes, CertificateTypes
 from zgw_consumers.forms import CertificateAdminForm
-from zgw_consumers.models import Certificate
+from zgw_consumers.models import Certificate, Service
 
 TEST_FILES = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")
 
@@ -19,7 +20,6 @@ class CertificateTests(TestCase):
         certificate_f = open(os.path.join(TEST_FILES, "test.certificate"), "r")
         key_f = open(os.path.join(TEST_FILES, "test.key"), "r")
 
-        # TODO make factory
         certificate = Certificate.objects.create(
             label="Test certificate",
             type=CertificateTypes.key_pair,
@@ -69,7 +69,6 @@ class CertificateTests(TestCase):
         # Valid key that belongs to another certificate
         key_f = open(os.path.join(TEST_FILES, "test2.key"), "r")
 
-        # TODO make factory
         certificate = Certificate.objects.create(
             label="Test certificate",
             type=CertificateTypes.key_pair,
@@ -86,7 +85,6 @@ class CertificateTests(TestCase):
         certificate_f = open(os.path.join(TEST_FILES, "test.certificate"), "r")
         key_f = open(os.path.join(TEST_FILES, "test1.key"), "r")
 
-        # TODO make factory
         certificate = Certificate.objects.create(
             label="Test certificate",
             type=CertificateTypes.key_pair,
@@ -101,7 +99,6 @@ class CertificateTests(TestCase):
 
     def test_valid_key_pair_missing_key(self):
         with open(os.path.join(TEST_FILES, "test.certificate"), "r") as certificate_f:
-            # TODO make factory
             certificate = Certificate.objects.create(
                 label="Test certificate",
                 type=CertificateTypes.key_pair,
@@ -109,3 +106,46 @@ class CertificateTests(TestCase):
             )
 
         self.assertIsNone(certificate.is_valid_key_pair())
+
+
+@temp_private_root()
+class ServiceWithCertificateTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        certificate_f = open(os.path.join(TEST_FILES, "test.certificate"), "r")
+        key_f = open(os.path.join(TEST_FILES, "test.key"), "r")
+
+        cls.certificate = Certificate.objects.create(
+            label="Test certificate",
+            type=CertificateTypes.key_pair,
+            public_certificate=File(certificate_f),
+            private_key=File(key_f),
+        )
+
+        certificate_f.close()
+        key_f.close()
+
+    def test_build_client_with_certificates(self):
+        oas_path = os.path.join(os.path.dirname(__file__), "schemas/drc.yaml")
+
+        with open(oas_path, "r") as oas_file:
+            service = Service.objects.create(
+                label="Test",
+                api_type=APITypes.drc,
+                api_root="https://foo.bar",
+                oas_file=File(oas_file, name="schema.yaml"),
+                server_certificate=self.certificate,
+            )
+
+        client = service.build_client()
+
+        with requests_mock.Mocker() as m:
+            m.get("https://foo.bar")
+            client.request("https://foo.bar", "enkelvoudiginformatieobject_list")
+            history = m.request_history
+
+        request_with_tls = history[0]
+
+        self.assertEqual(
+            self.certificate.public_certificate.path, request_with_tls.cert
+        )
