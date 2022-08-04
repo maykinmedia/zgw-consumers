87b6f7371c922ad698e4dc34006bad586c712032
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-08-04 10:49:47 +0200

    :sparkles: add DRF field for ServiceUrlField

--- zgw_consumers/apps.py
+++ zgw_consumers/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+from rest_framework import serializers
+
 
 class ZgwConsumersConfig(AppConfig):
     name = "zgw_consumers"
@@ -9,3 +11,12 @@ class ZgwConsumersConfig(AppConfig):
         from .models import lookups
 
         install_schema_fetcher_cache()
+        register_serializer_field()
+
+
+def register_serializer_field() -> None:
+    from .drf import fields as drf_fields
+    from .models import fields as model_fields
+
+    mapping = serializers.ModelSerializer.serializer_field_mapping
+    mapping[model_fields.ServiceUrlField] = drf_fields.ServiceUrlApiField
