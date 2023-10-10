242477896f09c435a87e905074ae92defeb94290
Viicos <65306057+Viicos@users.noreply.github.com>
2023-10-10 14:31:42 +0200

    Fix URL faker

--- zgw_consumers/test/factories.py
+++ zgw_consumers/test/factories.py
@@ -3,9 +3,22 @@ import factory
 from zgw_consumers.models import Service
 
 
+class UrlTrailingFaker(factory.Faker):
+    def __init__(self, **kwargs):
+        super().__init__("url", **kwargs)
+
+    def generate(self, extra_kwargs=None):
+        url: str = super().generate(extra_kwargs)
+        # faker generates them with a trailing slash, but let's make sure this stays true
+        # zgw_consumers.Service normalizes api_root to append missing trailing slashes
+        if not url.endswith("/"):
+            url = f"{url}/"
+        return url
+
+
 class ServiceFactory(factory.django.DjangoModelFactory):
     label = factory.Sequence(lambda n: f"API-{n}")
-    api_root = factory.Faker("url", schemes=["https"])
+    api_root = UrlTrailingFaker()
 
     class Meta:
         model = Service
