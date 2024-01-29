c3a0b24cc69699d375bc5f59faebbaec47827633
Steven Bal <steven@maykinmedia.nl>
2024-01-29 14:29:08 +0100

    :recycle: [#81] Replace get_paginated_results with pagination_helper

--- zgw_consumers/service.py
+++ zgw_consumers/service.py
@@ -1,70 +1,63 @@
-from typing import List, Optional, Type
-from urllib.parse import parse_qs, urlparse
+from typing import List, Optional, Type, TypedDict
 
+from ape_pie.client import APIClient
 from zds_client import Client
 
 from .api_models.base import ZGWModel, factory
 from .api_models.catalogi import Catalogus, InformatieObjectType
+from .client import build_client
 from .concurrent import parallel
 from .constants import APITypes
 from .models import Service
 
 
-def get_paginated_results(
-    client: Client,
-    resource: str,
-    minimum: Optional[int] = None,
-    test_func: Optional[callable] = None,
-    *args,
-    **kwargs
-) -> list:
-    request_kwargs = kwargs.get("request_kwargs", {})
-    request_params = request_kwargs.get("params", {})
+class PaginatedResponseData(TypedDict):
+    count: int
+    next: str
+    previous: str
+    results: list
 
-    results = []
-    response = client.list(resource, *args, **kwargs)
 
-    def _get_results(response):
-        _results = response["results"]
-        if test_func:
-            _results = [result for result in _results if test_func(result)]
-        return _results
+def pagination_helper(
+    client: APIClient,
+    paginated_data: PaginatedResponseData,
+    max_requests: Optional[int] = None,
+):
+    """
+    Fetch results from a paginated API endpoint, and optionally limit the number of
+    requests to perform when fetching new pages by specifying the ``max_requests`` argument
+    """
 
-    results += _get_results(response)
+    def _iter(_data, num_requests=0):
+        for result in _data["results"]:
+            yield result
+        if next_url := _data.get("next"):
+            if max_requests and num_requests >= max_requests:
+                return
+            response = client.get(next_url)
+            num_requests += 1
+            response.raise_for_status()
+            data = response.json()
+            yield from _iter(data, num_requests)
 
-    if minimum and len(results) >= minimum:
-        return results
-
-    while response.get("next"):
-        next_url = urlparse(response["next"])
-        query = parse_qs(next_url.query)
-        new_page = int(query["page"][0])
-
-        request_params["page"] = [new_page]
-        request_kwargs["params"] = request_params
-        kwargs["request_kwargs"] = request_kwargs
-
-        response = client.list(resource, *args, **kwargs)
-        results += _get_results(response)
-
-        if minimum and len(results) >= minimum:
-            return results
-
-    return results
+    return _iter(paginated_data)
 
 
 def _get_ztc_clients():
     services = Service.objects.filter(api_type=APITypes.ztc)
-    clients = [service.build_client() for service in services]
+    clients = [build_client(service) for service in services]
     return clients
 
 
 def _fetch_list(
-    resource: str, clients: List[Client], model: Type[ZGWModel]
+    path: str, clients: List[Client], model: Type[ZGWModel]
 ) -> List[ZGWModel]:
     def _fetch(client: Client):
-        results = get_paginated_results(client, resource)
-        return results
+        response = client.get(path)
+        response.raise_for_status()
+        data = response.json()
+        all_data = pagination_helper(client, data)
+        return all_data
 
     with parallel() as executor:
         resp_data = executor.map(_fetch, clients)
@@ -77,7 +70,7 @@ def get_catalogi(clients: List[Client] = None):
     if clients is None:
         clients = _get_ztc_clients()
 
-    return _fetch_list("catalogus", clients, Catalogus)
+    return _fetch_list("catalogussen", clients, Catalogus)
 
 
 def get_informatieobjecttypen(
@@ -90,7 +83,7 @@ def get_informatieobjecttypen(
         clients = _get_ztc_clients()
 
     catalogi = {cat.url: cat for cat in get_catalogi(clients=clients)}
-    iots = _fetch_list("informatieobjecttype", clients, InformatieObjectType)
+    iots = _fetch_list("informatieobjecttypen", clients, InformatieObjectType)
 
     # resolve relations
     for iot in iots:
