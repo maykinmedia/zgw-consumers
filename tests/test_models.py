4365497b277e7fff52efbdb2d5422d82b9029f0c
bart-maykin <bart.burgman@maykinmedia.nl>
2024-05-10 17:40:09 +0200

    :sparkles: [#1617] added feature to see if connection is alive in service admin list page

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,13 @@
 from django.core.exceptions import ValidationError
 
 import pytest
+import requests_mock
+from ape_pie import APIClient
 
+from zgw_consumers.client import ServiceConfigAdapter
 from zgw_consumers.constants import APITypes, AuthTypes
 from zgw_consumers.models import Service
+from zgw_consumers.test.factories import ServiceFactory
 
 
 def test_model_validation_with_oas_fields_enabled_none_provided(settings):
@@ -76,3 +80,45 @@ def test_model_validation_with_oas_fields_disabled_both_provided(settings):
         service.clean()
     except ValidationError:
         pytest.fail("OAS fields should be ignored")
+
+
+@pytest.mark.django_db
+def test_health_check_indication_service_model_badly_configured(settings):
+    service = ServiceFactory.create(
+        api_root="https://example.com/",
+        api_health_check_endpoint="foo",
+        auth_type=AuthTypes.zgw,
+        client_id="my-client-id",
+        secret="my-secret",
+    )
+    adapter = ServiceConfigAdapter(service)
+    client = APIClient.configure_from(adapter)
+
+    with requests_mock.Mocker() as m, client:
+        m.get(
+            "https://example.com/foo",
+            status_code=404,
+        )
+        service.refresh_from_db()
+        assert service.get_health_check_indication == False
+
+
+@pytest.mark.django_db
+def test_health_check_indication_service_model_correctly_configured(settings):
+    service = ServiceFactory.create(
+        api_root="https://example.com/",
+        api_health_check_endpoint="foo",
+        auth_type=AuthTypes.zgw,
+        client_id="my-client-id",
+        secret="my-secret",
+    )
+    adapter = ServiceConfigAdapter(service)
+    client = APIClient.configure_from(adapter)
+
+    with requests_mock.Mocker() as m, client:
+        m.get(
+            "https://example.com/foo",
+            status_code=200,
+        )
+        service.refresh_from_db()
+        assert service.get_health_check_indication == True
