9a06c1e2e8ed39fdd8f28debf5fde9519bff3b8f
bart-maykin <bart.burgman@maykinmedia.nl>
2024-06-11 10:42:55 +0200

    :art: [#1617] changed PrefixValidator to StartWithValidator

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,11 +2,11 @@ from django.core.exceptions import ValidationError
 
 import pytest
 
-from zgw_consumers.models.validators import IsNotUrlValidator, StartWithValidator
+from zgw_consumers.models.validators import NonUrlValidator, PrefixValidator
 
 
-def test_start_with_validator_return_value_false():
-    validator = StartWithValidator(prefix="/", return_value=False)
+def test_start_with_validator_starts_with_false():
+    validator = PrefixValidator(prefix="/", starts_with=False)
 
     assert validator.__call__("no_leading_slash") is None
 
@@ -16,8 +16,8 @@ def test_start_with_validator_return_value_false():
     assert "The given value cannot start with '/'" in exc_context.value
 
 
-def test_start_with_validator_return_value_true():
-    validator = StartWithValidator(prefix="/")
+def test_start_with_validator_starts_with_true():
+    validator = PrefixValidator(prefix="/")
 
     with pytest.raises(ValidationError) as exc_context:
         validator.__call__("no_leading_slash")
@@ -28,11 +28,11 @@ def test_start_with_validator_return_value_true():
 
 
 def test_is_not_url_validator():
-    validator = IsNotUrlValidator()
+    validator = NonUrlValidator()
 
     assert validator.__call__("some random text") is None
 
     with pytest.raises(ValidationError) as exc_context:
         assert validator.__call__("http://www.example.com")
 
-    assert "String cannot be a URL" in exc_context.value
+    assert "Value cannot be a URL" in exc_context.value
