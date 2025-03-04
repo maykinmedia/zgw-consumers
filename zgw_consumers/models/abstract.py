0529ed6b7ab8b1765f3bfaa9697e5271e7bf4a85
Steven Bal <steven@maykinmedia.nl>
2025-03-04 15:36:43 +0100

    :wastebasket: [#101] Remove code that was scheduled for deprecation in 1.0
    which includes any OAS related code and zds-client related code

--- zgw_consumers/models/abstract.py
+++ zgw_consumers/models/abstract.py
@@ -1,9 +1,5 @@
-from django.core.exceptions import ValidationError
-from django.core.validators import FileExtensionValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-
-from ..settings import get_setting
 
 
 class Service(models.Model):
@@ -11,40 +7,3 @@ class Service(models.Model):
 
     class Meta:
         abstract = True
-
-
-class RestAPIService(Service):
-    oas = models.URLField(
-        _("OAS url"), max_length=1000, blank=True, help_text=_("URL to OAS yaml file")
-    )
-    oas_file = models.FileField(
-        _("OAS file"),
-        blank=True,
-        help_text=_("OAS yaml file"),
-        upload_to="zgw-consumers/oas/",
-        validators=[FileExtensionValidator(["yml", "yaml"])],
-    )
-
-    class Meta:
-        abstract = True
-
-    def clean(self):
-        super().clean()
-
-        if get_setting("ZGW_CONSUMERS_IGNORE_OAS_FIELDS"):
-            return
-
-        if self.oas and self.oas_file:
-            raise ValidationError(
-                {
-                    "oas": _("Set either oas or oas_file, not both"),
-                    "oas_file": _("Set either oas or oas_file, not both"),
-                }
-            )
-        elif not self.oas and not self.oas_file:
-            raise ValidationError(
-                {
-                    "oas": _("Set either oas or oas_file"),
-                    "oas_file": _("Set either oas or oas_file"),
-                }
-            )
