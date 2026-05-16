6a584a90f76b48ff322c95f6b451f11203ce2c74
Sergei Maertens <sergei@maykinmedia.nl>
2026-05-16 17:19:00 +0200

    :alien: Update testapp for django-privates 4 support

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import copy
+
 import pytest
 from rest_framework.test import APIClient
 
@@ -8,11 +10,30 @@ def api_client(request) -> APIClient:
     return client
 
 
-@pytest.fixture()
+@pytest.fixture
 def temp_private_root(tmp_path, settings):
+    """
+    Set up a custom private root location that gets cleared up after tests.
+
+    Instead of using the in-memory approach of the unittest
+    privates.test.temp_private_root helper, we can rely on pytest cleaning up the
+    temporary directories here, so we use a real filesystem storage.
+    """
     tmpdir = tmp_path / "private-media"
     tmpdir.mkdir()
     location = str(tmpdir)
-    settings.PRIVATE_MEDIA_ROOT = location
+
+    _original = copy.deepcopy(settings.STORAGES)
+    new_storages = {
+        **_original,
+        "privates": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+            "OPTIONS": {
+                **_original["privates"].get("OPTIONS", {}),
+                "location": location,
+            },
+        },
+    }
+    settings.STORAGES = new_storages
     settings.SENDFILE_ROOT = location
     return settings
